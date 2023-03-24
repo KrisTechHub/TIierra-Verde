2f00e2756ce0087779cd2ef2eb77a1498f81589d
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-03-25 07:36:53 +0800

    fixed issue on top-div and nav upon scrolling up and down

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos < currentScrollPos) {
     document.getElementById("navigation-bar").style.visibility = "hidden";
+  } else if (document.body.scrollTop < 0 || document.documentElement.scrollTop < 10) {
+    document.getElementById("navigation-bar").style.top = "40px";
+    document.getElementById("navigation-bar").style.visibility = "visible";
+
   } else {
     document.getElementById("navigation-bar").style.visibility = "visible";
     document.getElementById("navigation-bar").style.top = "0px";
@@ -13,3 +17,6 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 //END
+
+
+
