0d3b96ccff36d584ff8028766be1c7c684020f05
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-03-26 14:33:46 +0800

    setting up js file for contact page, getting input values

--- main.js
+++ main.js
@@ -18,4 +18,37 @@ window.onscroll = function() {
 //END
 
 
+//CONTACT PAGE FUNCTION
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+      e.preventDefault(); //prevent the form from submitting
+
+      //get the input field values
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const comment = document.getElementById('input').value;
+
+      //validate the input fields   
+      if (!name || !email || !comment) {
+            alert("Please fill in all fields.");
+            return;
+      }
+
+      //send the form data to the server using and HTTP request
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', '/submit-form');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onload = function() {
+            if (xhr.status === 200) {
+                  alert("Thank you for contacting us!");
+                  form.reset();
+            } else {
+                  alert("There was a problem submitting the form. Please try again later.");
+            }
+      };
+      xhr.send(JSON.stringify({name, email, comment}));
+});
+
 
