b376094e2c2f084420f57ff50802cf4c27934fed
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-03-26 20:25:36 +0800

    modifying main.js for contact

--- main.js
+++ main.js
@@ -20,35 +20,35 @@ window.onscroll = function() {
 
 //CONTACT PAGE FUNCTION
 
-const form = document.getElementById('form');
+const form = document.getElementById('contact-form');
 
-form.addEventListener('submit', (e) => {
-      e.preventDefault(); //prevent the form from submitting
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting
 
-      //get the input field values
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const comment = document.getElementById('input').value;
+  // get the input field values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const comments = document.getElementById('input').value;
 
-      //validate the input fields   
-      if (!name || !email || !comment) {
-            alert("Please fill in all fields.");
-            return;
-      }
+  // validate the input fields
+  if (!name || !email || !comments) {
+    alert('Please fill in all fields.');
+    return;
+  }
 
-      //send the form data to the server using and HTTP request
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', '/submit-form');
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.onload = function() {
-            if (xhr.status === 200) {
-                  alert("Thank you for contacting us!");
-                  form.reset();
-            } else {
-                  alert("There was a problem submitting the form. Please try again later.");
-            }
-      };
-      xhr.send(JSON.stringify({name, email, comment}));
+  // send the form data to the server using an HTTP request
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/submit-contact-form');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      alert('Thanks for contacting us!');
+      form.reset();
+    } else {
+      alert('There was a problem submitting the form. Please try again later.');
+    }
+  };
+  xhr.send(JSON.stringify({ name, email, comments }));
 });
 
 
