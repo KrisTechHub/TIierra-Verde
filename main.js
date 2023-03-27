fa88887a7e6d20561732005bc12136162c218729
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-03-27 14:57:09 +0800

    resolving js issue for contact page

--- main.js
+++ main.js
@@ -19,31 +19,34 @@ window.onscroll = function() {
 
 
 //CONTACT PAGE FUNCTION
-const form = document.getElementById('submit');
+let form = document.getElementById("form");
+let names = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let comment = document.getElementById("input").value;
 
-submit.addEventListener('click', (event) => {
-  event.preventDefault(); // prevent the form from submitting
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("submitted form");
 
-  // get the input field values
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const comments = document.getElementById('input').value;
-
-  // create an object with the form data
-  const formData = { name, email, comments };
-
-  // send the form data to the server using an HTTP request
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/submit-form');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-      console.log(response.message);
-    } else {
-      console.error('There was a problem submitting the form.');
-    }
-  };
-  xhr.send(JSON.stringify(formData));
+  formValidation();
 });
+
+const formValidation = () => {
+  if (!names || !email || !comment) {
+    alert("Please fill in all fields.");
+    return;
+  } else {
+    acceptData();
+  }
+};
+
+let data = {};
+let acceptData = () => {
+  data["text"] = {names, email, comment};
+  console.log(`Email: ${email}, Name: ${names}`);
+  form.reset();
+}
+
+
+
 //END
