eb88761b4000acce3fe70ba0700f1c8dfff3bc42
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-02 17:27:31 +0800

    adding items on tabs

--- JS/landscaping.js
+++ JS/landscaping.js
@@ -1,83 +1,63 @@
 const menu = [
     {
       id: 1,
-      title: "buttermilk pancakes",
-      category: "breakfast",
-      price: 15.99,
-      img: "./images/item-1.jpeg",
-      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-    },
+      title: "tropical",
+      category: "landscape design",
+      img: "/images/landscaping/3.jpg",
+        },
     {
       id: 2,
       title: "diner double",
       category: "lunch",
-      price: 13.99,
-      img: "./images/item-2.jpeg",
-      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+      img: "/images/landscaping/4.jpg",
     },
     {
       id: 3,
       title: "godzilla milkshake",
       category: "shakes",
-      price: 6.99,
-      img: "./images/item-3.jpeg",
-      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+      img: "/images/landscaping/5.jpg",
     },
     {
       id: 4,
       title: "country delight",
       category: "breakfast",
-      price: 20.99,
-      img: "./images/item-4.jpeg",
-      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+      img: "/images/landscaping/6.jpg",
     },
     {
       id: 5,
       title: "egg attack",
       category: "lunch",
-      price: 22.99,
-      img: "./images/item-5.jpeg",
-      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+      img: "/images/landscaping/7.jpg",
     },
     {
       id: 6,
       title: "oreo dream",
       category: "shakes",
-      price: 18.99,
-      img: "./images/item-6.jpeg",
-      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+      img: "/images/landscaping/8.jpg",
     },
     {
       id: 7,
       title: "bacon overflow",
       category: "breakfast",
-      price: 8.99,
-      img: "./images/item-7.jpeg",
-      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+      img: "/images/landscaping/9.jpg",
     },
     {
       id: 8,
       title: "american classic",
       category: "lunch",
-      price: 12.99,
-      img: "./images/item-8.jpeg",
-      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+      img: "/images/landscaping/10.jpg",
     },
     {
       id: 9,
       title: "quarantine buddy",
       category: "shakes",
-      price: 16.99,
-      img: "./images/item-9.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      img: "/images/landscaping/11.jpg",
     },
     {
       id: 10,
       title: "bison steak",
       category: "dinner",
-      price: 22.99,
-      img: "./images/item-10.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      img: "/images/landscaping/12.jpg",
     },
   ];
   
@@ -96,13 +76,8 @@ const menu = [
         return `<article class="menu-item">
         <img src=${item.img} alt=${item.title} class="photo"/>
         <div class="item-info">
-          <header>
+            <p class="item-next">${item.category}</p>
             <h4>${item.title}</h4>
-            <h4 class="price">${item.price}</h4>
-          </header>
-          <p class="item-next">
-            ${item.desc}
-          </p>
         </div>
       </article>`;
     });
