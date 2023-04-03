750e10b6526655c431f23bd38423a8ddd763ac7f
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-03 18:39:11 +0800

    adding slideshow containers and contents

--- main.js
+++ main.js
@@ -48,3 +48,18 @@ let acceptData = () => {
   
 }
 //END
+
+
+// HOME PAGE SLIDESHOW FUNCTIONS
+const slides = document.querySelectorAll(".slideshow-slide");
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(() => {
+  showSlide(currentSlide + 1);
+}, 5000);
