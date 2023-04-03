2bab10235ec46969f547ec1a966f7d7943df0bd6
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-03 19:12:47 +0800

    adding slideshow contents and styles

--- main.js
+++ main.js
@@ -51,15 +51,4 @@ let acceptData = () => {
 
 
 // HOME PAGE SLIDESHOW FUNCTIONS
-const slides = document.querySelectorAll(".slideshow-slide");
-let currentSlide = 0;
 
-function showSlide(n) {
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].classList.add("active");
-}
-
-setInterval(() => {
-  showSlide(currentSlide + 1);
-}, 5000);
