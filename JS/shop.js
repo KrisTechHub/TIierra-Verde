cc787143b020ae8333aaa9be6ee99f0bfd6c9597
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-08 18:04:29 +0800

    adding price range input and output

---
+++ JS/shop.js
@@ -0,0 +1,33 @@
+//PRICE RANGE FUNCTIONALITY
+// Get range input element, output element, and list element
+const priceRange = document.getElementById('priceRange');
+const priceOutput = document.getElementById('priceOutput');
+const priceList = document.getElementById('priceList');
+
+// Add event listener for input changes
+priceRange.addEventListener('input', function() {
+  // Get range value
+  var rangeValue = parseInt(priceRange.value);
+
+  // Update output element
+  priceOutput.innerHTML = 'Price: &#8369;0 - &#8369;' + rangeValue;
+
+  // Get list items and loop through them
+  var listItems = priceList.getElementsByTagName('li');
+  for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+    var price = parseInt(listItem.getAttribute('data-price'));
+    
+    // Check if price is within range
+    if (price <= rangeValue) {
+      listItem.style.display = '';
+    } else {
+      listItem.style.display = 'none';
+    }
+  }
+});
+
+// Trigger input event to set initial output and filter
+priceRange.dispatchEvent(new Event('input'));
+
+//END
