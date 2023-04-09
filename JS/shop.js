b02e4254bed48a5b31e6fd78d7bd777a88677d07
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-09 12:58:19 +0800

    added primary js functions

--- JS/shop.js
+++ JS/shop.js
@@ -38,182 +38,182 @@ const plants = [
       id: 1,
       img: "/images/shop/1.jpg",
       name: "bamboo palm",
-      price: "&#8369;1,500",
+      price: "1,500",
       categories: ["all plants", "indoor plants", "tall", "pet"],
     },
     {
       id: 2,
       img: "/images/shop/2.jpg",
       name: "baston fern",
-      price: "&#8369;500",
+      price: "500",
       categories: ["all plants", "indoor plants", "table", "medium", "pet"],
     },
     {
       id: 3,
       img: "/images/shop/3.jpg",
       name: "breath easy bundle",
-      price: "&#8369;650",
+      price: "650",
       categories: ["all plants", "indoor plants", "specials", "table", "sets"],
     },
     {
       id: 4,
       img: "/images/shop/4.jpg",
       name: "brighten your day bundle",
-      price: "&#8369;750",
+      price: "750",
       categories: ["all plants", "indoor plants", "specials", "table", "sets"],
     },
     {
       id: 5,
       img: "/images/shop/5.jpg",
       name: "butterfly palm",
-      price: "&#8369;1,200",
+      price: "1,200",
       categories: ["all plants", "indoor plants", "tall", "pet"],
     },
     {
       id: 6,
       img: "/images/shop/19.jpg",
       name: "cactus",
-      price: "&#8369;800",
+      price: "800",
       categories: ["all plants", "medium"],
     },
     {
       id: 7,
       img: "/images/shop/6.jpg",
       name: "dracaena anita",
-      price: "&#8369;1,200",
+      price: "1,200",
       categories: ["all plants", "indoor plants", "tall"],
     },
     {
       id: 8,
       img: "/images/shop/7.jpg",
       name: "dwarf sansevieria | snake plant",
-      price: "&#8369;300",
+      price: "300",
       categories: ["all plants", "indoor plants", "specials", "table"],
     },
     {
       id: 9,
       img: "/images/shop/20.jpg",
       name: "fiddle-leaf fig",
-      price: "&#8369;3,500",
+      price: "3,500",
       categories: ["all plants", "medium", "tall"],
     },
     {
       id: 10,
       img: "/images/shop/8.jpg",
       name: "golden pothos",
-      price: "&#8369;300",
+      price: "300",
       categories: ["all plants", "indoor plants", "table"],
     },
     {
       id: 11,
       img: "/images/shop/17.jpg",
       name: "heartleaf philodendron",
-      price: "&#8369;500",
+      price: "500",
       categories: ["all plants", "table"],
     },
     {
       id: 12,
       img: "/images/shop/9.jpg",
       name: "heartleaf philodendron | special pot",
-      price: "&#8369;400",
+      price: "400",
       categories: ["all plants", "indoor plants", "specials", "table"],
     },
     {
       id: 13,
       img: "/images/shop/25.jpg",
       name: "licuala grandis",
-      price: "&#8369;1,500",
+      price: "1,500",
       categories: ["all plants", "tall", "pet"],
     },
     {
       id: 14,
       img: "/images/shop/21.jpg",
       name: "livistona",
-      price: "&#8369;1,200",
+      price: "1,200",
       categories: ["all plants", "indoor plants", "medium", "tall", "pet", "outdoor"],
     },
     {
       id: 15,
       img: "/images/shop/26.jpg",
       name: "macArthur palm",
-      price: "&#8369;1,500",
+      price: "1,500",
       categories: ["all plants", "tall"],
     },
     {
       id: 16,
       img: "/images/shop/11.jpg",
       name: "moonshine sansevieria | snake plant",
-      price: "&#8369;400",
+      price: "400",
       categories: ["all plants", "indoor plants", "specials", "table"],
     },
     {
       id: 17,
       img: "/images/shop/12.jpg",
       name: "peace lily",
-      price: "&#8369;1,300;",
+      price: "1,300;",
       categories: ["all plants", "indoor plants", "medium"],
     },
     {
       id: 18,
       img: "/images/shop/18.jpg",
       name: "peperomia obtusifolia",
-      price: "&#8369;500",
+      price: "500",
       categories: ["all plants", "table", "pet"],
     },
         {
       id: 19,
       img: "/images/shop/22.jpg",
       name: "philodendron giganteum",
-      price: "&#8369;1,500",
+      price: "1,500",
       categories: ["all plants", "indoor plants", "medium"],
     },
     {
       id: 20,
       img: "/images/shop/23.jpg",
       name: "philodendron selloum",
-      price: "&#8369;1,000",
+      price: "1,000",
       categories: ["all plants", "indoor plants", "medium"],
     },
     {
       id: 21,
       img: "/images/shop/27.jpg",
       name: "podocarpus macrophyllus 'Maki'",
-      price: "&#8369;1,000",
+      price: "1,000",
       categories: ["all plants", "tall", "outdoor"],
     },
     {
       id: 22,
       img: "/images/shop/13.jpg",
       name: "potted plants collection",
-      price: "&#8369;500",
+      price: "500",
       categories: ["all plants", "indoor plants", "specials", "table", "gifts"],
     },
     {
       id: 23,
       img: "/images/shop/24.jpg",
       name: "rhapis excelsa",
-      price: "&#8369;1,000",
+      price: "1,000",
       categories: ["all plants", "indoor plants", "medium", "pet"],
     },
     {
       id: 24,
       img: "/images/shop/14.jpg",
       name: "snake plant bundle",
-      price: "&#8369;650",
+      price: "650",
       categories: ["all plants", "indoor plants", "specials", "table", "sets"],
     },
     {
       id: 25,
       img: "/images/shop/15.jpg",
       name: "two hearts bundle",
-      price: "&#8369;750",
+      price: "750",
       categories: ["all plants", "indoor plants", "specials", "table", "sets"],
     },
     {
       id: 26,
       img: "/images/shop/28.jpg",
       name: "various hebrs",
-      price: "&#8369;450",
+      price: "450",
       categories: ["all plants", "herbs"],
     },
   ];
@@ -223,7 +223,21 @@ const btnContainer = document.querySelector(".btn-container");
 
 //display all items when page  loads
 window.addEventListener("DOMContentLoaded", function() {
-    
+    displayPlantItems(plants);
 });
 
-displayPlantItems = (plantItems)
+displayPlantItems = (plantItems) => {
+    let displayPlants = plantItems.map(function(item) {
+
+        return `<article class="plant-item">
+            <img src=${item.img} alt=${item.name} class="photo"/>
+            <div class="item-info">
+                <p class="item-next">${item.name}</p>
+                <p data-price=${item.price}>&#8369;${item.price}</p>
+            </div>
+        </article>`;        
+    });
+    displayPlants = displayPlants.join("");
+
+    sectionCenter.innerHTML = displayPlants;
+}
