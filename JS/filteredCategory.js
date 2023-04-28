1a745133126fa6efe1eeb70f543cbfc9af68a753
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-04-28 15:42:06 +0800

    functions:

--- JS/filteredCategory.js
+++ JS/filteredCategory.js
@@ -7,19 +7,19 @@ for (const product of products) {
         const isExists = categories.includes(cat)
         if (!isExists) {
             categories.push(cat)
-        }
+        } 
     }
 }
 
 const categorySelect = document.querySelector('#category')
 
-for (const cat of categories) {
-    const option = document.createElement("a")
-    option.append(cat)
-    categorySelect.appendChild(option)
-
+    for (const cat of categories) {
+        const option = document.createElement("a")
+        option.append(cat)
+        categorySelect.appendChild(option)
 
 }
+
 
 const showDiv = document.querySelector('.showDiv')
 
@@ -28,9 +28,8 @@ showDiv.addEventListener("click", (e) => {
         categorySelect.style.display = "none";
     } else {
         categorySelect.style.display = "flex";
-        categorySelect.style.flexdirection = "column";
+        categorySelect.style.flexDirection = "column";
     };
-
 
 
 });
