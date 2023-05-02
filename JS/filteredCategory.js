402734e807720da7df7081b4c3753192ac05e780
KrisTechHub <114846706+KrisTechHub@users.noreply.github.com>
2023-05-02 16:01:01 +0800

    first

--- JS/filteredCategory.js
+++ JS/filteredCategory.js
@@ -1,27 +1,4 @@
   // Display initial products and pagination on page load
-
-const categories = [];
-for (const product of products) {
-    for (const cat of product.categories) {
-        const isExists = categories.includes(cat)
-        if (!isExists) {
-            categories.push(cat)
-        } 
-    }
-};
-
-
-const categorySelect = document.querySelector('#category')
-for (const cat of categories) {
-    const option = document.createElement("button")
-    option.append(cat)
-    categorySelect.appendChild(option)
-
-    option.classList.add("filterbtn");
-    option.setAttribute("data-id", cat)
-};
-
-
 const showDiv = document.querySelector('.showDiv')
 showDiv.addEventListener("click", () => {
     if (categorySelect.style.display === "flex") {
@@ -33,22 +10,59 @@ showDiv.addEventListener("click", () => {
 });
 
 
-const filterBtn = document.querySelector(".filter-btn");
-filterBtn?.addEventListener("click", (e) => {
-        const category = e.currentTarget.dataset.id;
-        const plantCategory = products.filter(function(plantItem) {
-            if (plantItem.categories === category) {
-                console.log("clicked");
-                return plantItem;
-            } 
-        });
+
+const categorySelect = document.querySelector('#category');
+const categories = [];
+for (const product of products) {
+    for (const cat of product.categories) {
+        const isExists = categories.includes(cat)
+        if (!isExists) {
+            categories.push(cat)
+        };
 
 
-        if (category === "all") {
-        displayPlantItems(plants);
-        } else {
-        displayPlantItems(plantCategory);
+    }
+};
+
+
+
+for (const cat of categories) {
+    const optionBtn = document.createElement("button")
+    optionBtn.append(cat)
+    categorySelect.appendChild(optionBtn)
+
+    optionBtn.classList.add("filterbtn");
+    optionBtn.setAttribute("data-id", cat);
+    optionBtn.setAttribute("type", "button");
+    optionBtn.setAttribute("onclick", filteredProds(catName));
+};
+
+
+function filteredProds(catName) {
+    let filteredProds = []
+    for (let index = 0; index < products.length; index++) {
+        if (products[index].cat === "pet") {
+            filteredProds.push(products[i])
         }
-        console.log("working");
+    }
+    filteredProds(catName);
+}
 
-});
+const filterBtn = categorySelect.querySelectorAll(".filterbtn");
+
+filterBtn.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+      const category = e.currentTarget.dataset.id;
+      const plantCategory = plants.filter(function(plantItem) {
+        if (plantItem.category === category) {
+          return plantItem;
+        } 
+      });
+
+      if (category === "all") {
+        displayPlantItems(plants);
+      } else {
+        displayPlantItems(plantCategory);
+      }
+    });
+  });
